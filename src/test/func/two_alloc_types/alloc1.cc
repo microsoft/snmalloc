28a28bb1ed26c7f239a38643d6c73b63f2824216
Matthew Parkinson <mattpark@microsoft.com>
2019-01-23 11:08:21 +0000

    Simple example of using two allocators in one application.
    Example to simulate using two allocators in one application.  This will
    be useful for scenarios in openenclave.

---
+++ src/test/func/two_alloc_types/alloc1.cc
@@ -0,0 +1,10 @@
+#undef IS_ADDRESS_SPACE_CONSTRAINED
+#define OPEN_ENCLAVE
+#define OPEN_ENCLAVE_SIMULATION
+#define USE_RESERVE_MULTIPLE 1
+#define NO_BOOTSTRAP_ALLOCATOR
+#define IS_ADDRESS_SPACE_CONSTRAINED
+#define SNMALLOC_NAME_MANGLE(a) enclave_##a
+// Redefine the namespace, so we can have two versions.
+#define snmalloc snmalloc_enclave
+#include "../../../override/malloc.cc"
