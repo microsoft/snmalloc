29e4bdd29d365f44557ac3469cce24defe62ba6b
Roy Schuster <roy.schuster@hotmail.com>
2019-04-12 12:32:37 +0100

    Splitting EXPOSE_EXTERNAL_ALLOCATOR_API to EXPOSE_EXTERNAL_PAGEMAP and EXPOSE_EXTERNAL_RESERVE. Only the shim can be affected by passing the new pre-processor definitions to cmake.

--- src/test/func/two_alloc_types/alloc1.cc
+++ src/test/func/two_alloc_types/alloc1.cc
@@ -4,9 +4,7 @@
 #define USE_RESERVE_MULTIPLE 1
 #define NO_BOOTSTRAP_ALLOCATOR
 #define IS_ADDRESS_SPACE_CONSTRAINED
-#ifndef SNMALLOC_EXPOSE_PAGEMAP
-#  define SNMALLOC_EXPOSE_PAGEMAP
-#endif
+#define SNMALLOC_EXPOSE_PAGEMAP
 #define SNMALLOC_NAME_MANGLE(a) enclave_##a
 // Redefine the namespace, so we can have two versions.
 #define snmalloc snmalloc_enclave
