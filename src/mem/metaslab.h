22d6a30e1f4c5c90de4341fb2cc25e95749426a2
Nathaniel Filardo <nwf20@cl.cam.ac.uk>
2019-07-15 16:14:36 +0100

    CHERI: Don't POISON capabilities
    They can't be xor'd like this and preserve tag provenance.

--- src/mem/metaslab.h
+++ src/mem/metaslab.h
@@ -109,7 +109,7 @@ namespace snmalloc
     /// simple corruptions.
     static SNMALLOC_FAST_PATH void store_next(void* p, void* head)
     {
-#ifndef CHECK_CLIENT
+#if !defined(CHECK_CLIENT) || defined(__CHERI_PURE_CAPABILITY__)
       *static_cast<void**>(p) = head;
 #else
       *static_cast<void**>(p) = head;
@@ -121,7 +121,7 @@ namespace snmalloc
     /// In Debug checks for simple corruptions.
     static SNMALLOC_FAST_PATH void* follow_next(void* node)
     {
-#ifdef CHECK_CLIENT
+#if defined(CHECK_CLIENT) && !defined(__CHERI_PURE_CAPABILITY__)
       uintptr_t next = *static_cast<uintptr_t*>(node);
       uintptr_t chk = *(static_cast<uintptr_t*>(node) + 1);
       if ((next ^ chk) != POISON)
