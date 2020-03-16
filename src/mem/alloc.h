ffb7b82643a8671e6f6f517e24901350d8da7266
Matthew Parkinson <mattpark@microsoft.com>
2020-03-16 11:11:15 +0000

    Clang format again.

--- src/mem/alloc.h
+++ src/mem/alloc.h
@@ -1277,16 +1277,15 @@ namespace snmalloc
         return;
       }
 
-      remote_dealloc_slow(target, p , sizeclass);
+      remote_dealloc_slow(target, p, sizeclass);
     }
 
     SNMALLOC_SLOW_PATH
     void remote_dealloc_slow(
       RemoteAllocator* target, void* offseted, sizeclass_t sizeclass)
     {
-      MEASURE_TIME(remote_dealloc, 4, 16);
       SNMALLOC_ASSERT(target->id() != id());
-
+      
       // Now that we've established that we're in the slow path (if we're a
       // real allocator, we will have to empty our cache now), check if we are
       // a real allocator and construct one if we aren't.
