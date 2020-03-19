f6d3e6be80208fb001019a926de56d8e43fe3507
Amaury Chamayou <amchamay@microsoft.com>
2020-03-19 20:25:32 +0000

    Factor what can be into common scripts

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-set -ex
-
-export CC=/usr/bin/clang-9
-export CXX=/usr/bin/clang++-9
-
 mkdir build
 cd build
-cmake -GNinja -DSNMALLOC_CI_BUILD=ON -DSNMALLOC_QEMU_WORKAROUND=ON ..
+
+CMAKE_CXX_FLAGS="-std=c++17"
+if [[ "$CXX" == clang++* ]]; then
+    CMAKE_CXX_FLAGS+=" -stdlib=libstdc++";
+fi
+
+cmake \
+  $CMAKE_ARGS \
+  -GNinja \
+  -DSNMALLOC_CI_BUILD=ON \
+  -DSNMALLOC_RUST_SUPPORT=ON \
+  -DSNMALLOC_QEMU_WORKAROUND=ON \
+  -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+  -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" \
+  ..
 ninja
--- ci/scripts/test.sh
+++ ci/scripts/test.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-set -ex
-
 cd build
-ctest -j4
+if [ $SELF_HOST = false ]; then
+    ctest -j 4 --output-on-failure -C $BUILD_TYPE
+else
+    sudo cp libsnmallocshim.so libsnmallocshim-1mib.so /usr/local/lib/
+    ninja clean
+    LD_PRELOAD=/usr/local/lib/libsnmallocshim.so ninja
+    ninja clean
+    LD_PRELOAD=/usr/local/lib/libsnmallocshim-1mib.so ninja
+fi
