e0ce372b129cea0b9b070ca0ef4b084f21d86ccf
Matthew Parkinson <mattpark@microsoft.com>
2020-03-19 14:56:25 +0000

    Fixup

--- src/aal/aal_x86_sgx.h
+++ src/aal/aal_x86_sgx.h
@@ -7,8 +7,12 @@
 #  include <emmintrin.h>
 #endif
 
-// Only support 64bit mode for SGX.
-#define SNMALLOC_VA_BITS_64
+#if defined(__amd64__) || defined(__x86_64__) || defined(_M_X64) || \
+  defined(_M_AMD64)
+#  define SNMALLOC_VA_BITS_64
+#else
+#  define SNMALLOC_VA_BITS_32
+#endif
 
 namespace snmalloc
 {
