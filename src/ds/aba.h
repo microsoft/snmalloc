9412233bbd1d258f1b5ed763f929712cdf80cb91
Paul Liétar <paul@lietar.net>
2020-04-15 18:12:17 +0200

    clang-format

--- src/ds/aba.h
+++ src/ds/aba.h
@@ -99,7 +99,8 @@ namespace snmalloc
           (__int64)value,
           (__int64*)&old);
 #  else
-#    if defined(__GNUC__) && defined(SNMALLOC_VA_BITS_64) && !defined(__GCC_HAVE_SYNC_COMPARE_AND_SWAP_16)
+#    if defined(__GNUC__) && defined(SNMALLOC_VA_BITS_64) && \
+      !defined(__GCC_HAVE_SYNC_COMPARE_AND_SWAP_16)
 #error You must compile with -mcx16 to enable 16-byte atomic compare and swap.
 #    endif
 
