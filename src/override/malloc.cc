497f9d9386710735d2fef9008b82b5f52ce8e2d9
SchrodingerZhu <i@zhuyi.fan>
2020-05-19 13:46:40 +0800

    add static library (#189)
    * add static library
    
    * leave static library on by default
    
    * fix 1mib layout
    
    * code reviews

--- src/override/malloc.cc
+++ src/override/malloc.cc
@@ -9,8 +9,12 @@ using namespace snmalloc;
 #ifndef SNMALLOC_EXPORT
 #  define SNMALLOC_EXPORT
 #endif
-
-#ifndef SNMALLOC_NAME_MANGLE
+#ifdef SNMALLOC_STATIC_LIBRARY_PREFIX
+#  define __SN_CONCAT(a, b) a##b
+#  define __SN_EVALUATE(a, b) __SN_CONCAT(a, b)
+#  define SNMALLOC_NAME_MANGLE(a) \
+    __SN_EVALUATE(SNMALLOC_STATIC_LIBRARY_PREFIX, a)
+#elif !defined(SNMALLOC_NAME_MANGLE)
 #  define SNMALLOC_NAME_MANGLE(a) a
 #endif
 
