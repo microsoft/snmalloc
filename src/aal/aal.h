4b7ee7808b82a8961dd99cd510636953d0d68a46
Nathaniel Filardo <nwf20@cl.cam.ac.uk>
2020-05-20 20:21:11 +0000

    AAL: new StrictProvenance feature flag

--- src/aal/aal.h
+++ src/aal/aal.h
@@ -36,6 +36,12 @@ namespace snmalloc
      * This architecture cannot access cpu cycles counters.
      */
     NoCpuCycleCounters = (1 << 1),
+    /**
+     * This architecture enforces strict pointer provenance; we bound the
+     * pointers given out on malloc() and friends and must, therefore retain
+     * internal high-privilege pointers for recycling memory on free().
+     */
+    StrictProvenance = (1 << 2),
   };
 
   /**
