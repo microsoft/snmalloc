41473ef2e711ef29a5ab7c6bd5752b5edf952891
David Carlier <devnexen@gmail.com>
2020-06-28 21:49:03 +0000

    MAP_NORESERVE usage

--- src/pal/pal_haiku.h
+++ src/pal/pal_haiku.h
@@ -30,6 +30,55 @@ namespace snmalloc
       SNMALLOC_ASSERT(is_aligned_block<page_size>(p, size));
       posix_madvise(p, size, POSIX_MADV_DONTNEED);
     }
+
+    /**
+     *  OS specific function for zeroing memory
+     *  using MAP_NORESERVE for explicit over commit appliance.
+     */
+    template<bool page_aligned = false>
+    void zero(void* p, size_t size)
+    {
+      if (page_aligned || is_aligned_block<page_size>(p, size))
+      {
+        SNMALLOC_ASSERT(is_aligned_block<page_size>(p, size));
+        void* r = mmap(
+          p,
+          size,
+          PROT_READ | PROT_WRITE,
+          MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED | MAP_NORESERVE,
+          -1,
+          0);
+
+        if (r != MAP_FAILED)
+          return;
+      }
+
+      bzero(p, size);
+    }
+
+    /**
+     * Reserve memory using MAP_NORESERVE for explicit
+     * over commit applicance.
+     */
+    std::pair<void*, size_t> reserve_at_least(size_t size)
+    {
+      constexpr size_t min_size =
+        bits::is64() ? bits::one_at_bit(32) : bits::one_at_bit(28);
+      auto size_request = bits::max(size, min_size);
+
+      void* p = mmap(
+        nullptr,
+        size_request,
+        PROT_READ | PROT_WRITE,
+        MAP_PRIVATE | MAP_ANONYMOUS | MAP_NORESERVE,
+        -1,
+        0);
+
+      if (p == MAP_FAILED)
+        error("Out of memory");
+
+      return {p, size_request};
+    }
   };
 } // namespace snmalloc
 #endif
