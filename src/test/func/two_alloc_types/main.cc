37ffb72d09b626002d2a1d8b6283e1938a397d26
Nathaniel Filardo <nfilardo@microsoft.com>
2020-08-01 00:22:24 +0100

    WIP: extend func/two_alloc_types to lightly test foreign allocator support
    Cause the host allocator to percieve the enclave allocator as foreign and call
    the provided free()-ing trampoline.

--- src/test/func/two_alloc_types/main.cc
+++ src/test/func/two_alloc_types/main.cc
@@ -23,10 +23,22 @@ extern "C" void host_free(void*);
 extern "C" void* enclave_malloc(size_t);
 extern "C" void enclave_free(void*);
 
+void host_to_enclave_free(void* arg, void* p)
+{
+  // No security domain transition, but hand off to enclave allocator
+  UNUSED(arg);
+
+  std::cout << "HTEF " << p << std::endl;
+
+  enclave_free(p);
+}
+
 extern "C" void*
 enclave_snmalloc_pagemap_global_get(snmalloc::PagemapConfig const**);
 extern "C" void*
 host_snmalloc_pagemap_global_get(snmalloc::PagemapConfig const**);
+extern "C" void*
+host_snmalloc_designate_foreign(void*, size_t, struct ForeignAllocator*);
 
 using namespace snmalloc;
 int main()
@@ -46,6 +58,10 @@ int main()
   std::cout << "Allocated region " << oe_base << " - " << oe_end << std::endl;
 
   // Call these functions to trigger asserts if the cast-to-self doesn't work.
+  //
+  // Note that these check *type* compatibility but do not actually unify the
+  // two Pagemaps: that is, the enclave and host allocators continue to have
+  // separate pagemaps.
   const PagemapConfig* c;
   enclave_snmalloc_pagemap_global_get(&c);
   host_snmalloc_pagemap_global_get(&c);
@@ -58,4 +74,17 @@ int main()
 
   host_free(a);
   enclave_free(b);
+
+  /*
+   * Designate the OE space as a foreign allocator in the host allocator's maps
+   */
+  struct ForeignAllocator* fa_enclave =
+    static_cast<ForeignAllocator*>(host_malloc(sizeof fa_enclave));
+  fa_enclave->arg = NULL;
+  fa_enclave->free = host_to_enclave_free;
+
+  host_snmalloc_designate_foreign(oe_base, size, fa_enclave);
+
+  auto x = enclave_malloc(128);
+  host_free(x);
 }
