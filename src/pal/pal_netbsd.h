f8be3ffa1d85624926ffd6f00ecf161857b03402
David Carlier <devnexen@gmail.com>
2020-08-27 20:35:14 +0000

    netbsd: reserve_aligned making the segment shared to fix an unit test crashing.

--- src/pal/pal_netbsd.h
+++ src/pal/pal_netbsd.h
@@ -24,6 +24,34 @@ namespace snmalloc
      * add new features that they should add any required feature flags.
      */
     static constexpr uint64_t pal_features = PALBSD_Aligned::pal_features;
+
+    /**
+     * Reserve memory at a specific alignment.
+     * Oddily, it appears it needs to be shared segment
+     * as it should not need to but func-memory-16 crashes otherwise.
+     */
+    template<bool committed>
+    void* reserve_aligned(size_t size) noexcept
+    {
+      // Alignment must be a power of 2.
+      SNMALLOC_ASSERT(size == bits::next_pow2(size));
+      SNMALLOC_ASSERT(size >= minimum_alloc_size);
+
+      size_t log2align = bits::next_pow2_bits(size);
+
+      void* p = mmap(
+        nullptr,
+        size,
+        PROT_READ | PROT_WRITE,
+        MAP_SHARED | MAP_ANONYMOUS | MAP_ALIGNED(log2align),
+        -1,
+        0);
+
+      if (p == MAP_FAILED)
+        PALBSD_Aligned<PALNetBSD>::error("Out of memory");
+
+      return p;
+    }
   };
 } // namespace snmalloc
 #endif
