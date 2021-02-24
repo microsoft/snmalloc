fda17fe6fc7fdd6ba17b48feb68da4feba8dfe11
Matthew Parkinson <mattpark@microsoft.com>
2021-02-24 11:20:30 +0000

    Clangformat.

--- src/pal/pal_timer_default.h
+++ src/pal/pal_timer_default.h
@@ -15,8 +15,8 @@ namespace snmalloc
   public:
     static uint64_t time_in_ms()
     {
-      auto time =
-        static_cast<uint64_t>(std::chrono::duration_cast<std::chrono::milliseconds>(
+      auto time = static_cast<uint64_t>(
+        std::chrono::duration_cast<std::chrono::milliseconds>(
           std::chrono::steady_clock::now().time_since_epoch())
           .count());
 
