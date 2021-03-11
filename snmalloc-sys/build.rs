f4e4de7b49ab83be09e89703fda4cee004131643
Ryan Clanton <ryanc@Insightec.com>
2021-03-11 13:18:41 -0500

    use cc instead of cmake

--- snmalloc-sys/build.rs
+++ snmalloc-sys/build.rs
@@ -1,62 +1,75 @@
-use cmake::Config;
-
 fn main() {
-    let mut cfg = &mut Config::new("snmalloc");
-
-    let build_type = if cfg!(feature = "debug") {
-        "Debug"
-    } else {
-        "Release"
-    };
-
-    cfg = cfg.define("SNMALLOC_RUST_SUPPORT", "ON")
-        .profile(build_type);
+    let mut build = cc::Build::new();
+    build.include("snmalloc/src");
+    build.file("snmalloc/src/override/rust.cc".to_string());
+    build.flag_if_supported("/O2");
+    build.flag_if_supported("/W4");
+    build.flag_if_supported("/WX");
+    build.flag_if_supported("/wd4127");
+    build.flag_if_supported("/wd4324");
+    build.flag_if_supported("/wd4201");
+    build.flag_if_supported("/Ob2");
+    build.flag_if_supported("/DNDEBUG");
+    build.flag_if_supported("/EHsc");
+    build.flag_if_supported("/std:c++17");
+    build.flag_if_supported("-O3");
+    build.flag_if_supported("-Wc++17-extensions");
+    build.flag_if_supported("-std=c++1z");
+    build.flag_if_supported("-std=gnu++1z");
+    build.flag_if_supported("-mcx16");
+	build.flag_if_supported("-fno-exceptions");
+	build.flag_if_supported("-fno-rtti");
+	build.flag_if_supported("-g");
+	build.flag_if_supported("-fomit-frame-pointer");
+    build.cpp(true);
+    build.debug(false);
 
     let triple = std::env::var("TARGET").unwrap();
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").expect("target_os not defined!");
+    let target_env = std::env::var("CARGO_CFG_TARGET_ENV").expect("target_env not defined!");
+	let target_family = std::env::var("CARGO_CFG_TARGET_FAMILY").expect("target family not set");
+
+	
     if triple.contains("android") {
-        if let Ok(ndk) = std::env::var("ANDROID_NDK") {
-            cfg = cfg.define("CMAKE_TOOLCHAIN_FILE", format!("{}/build/cmake/android.toolchain.cmake", ndk));
-        } else {
-            eprintln!("please set ANDROID_NDK environment variable");
-            std::process::abort();
-        }
-
-        if let Ok(platform) = std::env::var("ANDROID_PLATFORM") {
-            cfg = cfg.define("ANDROID_PLATFORM", platform);
-        }
-
         if cfg!(feature = "android-lld") {
-            cfg = cfg.define("ANDROID_LD", "lld");
+            build.define("ANDROID_LD", "lld");
         }
 
         if cfg!(feature = "android-shared-stl") {
-            println!("cargo:rustc-link-lib=dylib=c++_shared");
-            cfg = cfg.define("ANDROID_STL", "c++_shared");
+            build.define("ANDROID_STL", "c++_shared");
         }
 
         if triple.contains("aarch64") {
-            cfg = cfg.define("ANDROID_ABI", "arm64-v8a");
+            build.define("ANDROID_ABI", "arm64-v8a");
         } else if triple.contains("armv7") {
-            cfg = cfg.define("ANDROID_ABI", "armeabi-v7a")
-                .define("ANDROID_ARM_MODE", "arm");
+            build.define("ANDROID_ABI", "armeabi-v7a");
+            build.define("ANDROID_ARM_MODE", "arm");
         } else if triple.contains("x86_64") {
-            cfg = cfg.define("ANDROID_ABI", "x86_64");
+            build.define("ANDROID_ABI", "x86_64");
         } else if triple.contains("i686") {
-            cfg = cfg.define("ANDROID_ABI", "x86_64");
+            build.define("ANDROID_ABI", "x86_64");
         } else if triple.contains("neon") {
-            cfg = cfg.define("ANDROID_ABI", "armeabi-v7a with NEON")
+            build.define("ANDROID_ABI", "armeabi-v7a with NEON");
         } else if triple.contains("arm") {
-            cfg = cfg.define("ANDROID_ABI", "armeabi-v7a");
+            build.define("ANDROID_ABI", "armeabi-v7a");
         }
     }
-
-    if cfg!(all(windows, target_env = "msvc")) {
-        cfg = cfg.define("CMAKE_CXX_FLAGS_RELEASE", "/O2 /Ob2 /DNDEBUG /EHsc");
-        cfg = cfg.define("CMAKE_C_FLAGS_RELEASE", "/O2 /Ob2 /DNDEBUG /EHsc");
+	
+    if target_os=="windows" && target_env == "gnu" {
+        build.define("CMAKE_SH", "CMAKE_SH-NOTFOUND");
+        if cfg!(feature = "local_dynamic_tls") {
+            build.flag_if_supported("-ftls-model=local-dynamic");
+        } else {
+            build.flag_if_supported("-ftls-model=initial-exec");
+        }
     }
-
-    if cfg!(all(windows, target_env = "gnu")) {
-        cfg = cfg.define("CMAKE_SH", "CMAKE_SH-NOTFOUND");
+	
+    if target_family == "unix" && target_os != "haiku" {
+        if cfg!(feature = "local_dynamic_tls") {
+            build.flag_if_supported("-ftls-model=local-dynamic");
+        } else {
+            build.flag_if_supported("-ftls-model=initial-exec");
+        }
     }
 
     let target = if cfg!(feature = "1mib") {
@@ -68,77 +81,51 @@ fn main() {
     };
 
     if cfg!(feature = "native-cpu") {
-        cfg = cfg.define("SNMALLOC_OPTIMISE_FOR_CURRENT_MACHINE", "ON")
+        build.define("SNMALLOC_OPTIMISE_FOR_CURRENT_MACHINE", "ON");
+		build.flag_if_supported("-march=native");
     }
 
     if cfg!(feature = "stats") {
-        cfg = cfg.define("USE_SNMALLOC_STATS", "ON")
+        build.define("USE_SNMALLOC_STATS", "ON");
     }
 
     if cfg!(feature = "qemu") {
-        cfg = cfg.define("SNMALLOC_QEMU_WORKAROUND", "ON")
+        build.define("SNMALLOC_QEMU_WORKAROUND", "ON");
     }
 
-    let mut dst = if cfg!(feature = "cache-friendly") {
-        cfg.define("CACHE_FRIENDLY_OFFSET", "64").build_target(target).build()
-    } else {
-        cfg.build_target(target).build()
-    };
+    if cfg!(feature = "cache-friendly") {
+        build.define("CACHE_FRIENDLY_OFFSET", "64");
+    }
 
-    dst.push("./build");
+    build.compile(target);
 
-    println!("cargo:rustc-link-lib={}", target);
+    if cfg!(feature = "android-shared-stl") {
+        println!("cargo:rustc-link-lib=dylib=c++_shared");
+    }
 
-    if cfg!(all(windows, target_env = "msvc")) {
+    if target_env == "msvc" {
         println!("cargo:rustc-link-lib=dylib=mincore");
-        println!("cargo:rustc-link-search=native={}/{}", dst.display(), build_type);
-    } else {
-        println!("cargo:rustc-link-search=native={}", dst.display());
     }
 
-    if cfg!(all(windows, target_env = "gnu")) {
-        let stdout = std::process::Command::new("gcc")
-            .args(&["-print-search-dirs"])
-            .output().unwrap_or_else(|_| {
-            eprintln!("Cannot run gcc.exe");
-            std::process::abort();
-        })
-            .stdout;
-
-        let outputs = String::from_utf8(stdout)
-            .unwrap_or_else(|_| {
-                eprintln!("gcc output contains non-utf8 characters");
-                std::process::abort();
-            });
-
-        outputs.lines()
-            .filter(|line| line.starts_with("libraries: ="))
-            .map(|line| line.split_at("libraries: =".len()).1)
-            .flat_map(|line| line.split(";"))
-            .for_each(|path| {
-                println!("cargo:rustc-link-search=native={}", path);
-            });
-
-        println!("cargo:rustc-link-lib=dylib=stdc++");
-        println!("cargo:rustc-link-lib=dylib=atomic");
-        println!("cargo:rustc-link-lib=dylib=winpthread");
-        println!("cargo:rustc-link-lib=dylib=gcc_s");
-    }
-
-    if cfg!(target_os = "macos") {
-        println!("cargo:rustc-link-lib=dylib=c++");
-    }
-
-    if cfg!(target_os = "openbsd") {
-        println!("cargo:rustc-link-lib=dylib=c++");
-    }
-
-    if cfg!(target_os = "freebsd") {
-        println!("cargo:rustc-link-lib=dylib=c++");
-    }
-
-    if cfg!(target_os = "linux") {
+    if target_os=="windows" && target_env == "gnu" {
         println!("cargo:rustc-link-lib=dylib=stdc++");
         println!("cargo:rustc-link-lib=dylib=atomic");
     }
+
+    if target_os == "macos" {
+        println!("cargo:rustc-link-lib=dylib=c++");
+    }
+
+    if target_os == "openbsd" {
+        println!("cargo:rustc-link-lib=dylib=c++");
+    }
+
+    if target_os == "freebsd" {
+        println!("cargo:rustc-link-lib=dylib=c++");
+    }
+
+    if target_os == "linux" {
+        println!("cargo:rustc-link-lib=dylib=stdc++");
+        println!("cargo:rustc-link-lib=dylib=atomic");
+    };
 }
--- snmalloc-sys/src/lib.rs
+++ snmalloc-sys/src/lib.rs
@@ -5,8 +5,8 @@ use core::ffi::c_void;
 use libc::size_t;
 
 extern "C" {
-    /// Allocate the memory with the given alignment and size. 
-    /// On success, it returns a pointer pointing to the required memory address. 
+    /// Allocate the memory with the given alignment and size.
+    /// On success, it returns a pointer pointing to the required memory address.
     /// On failure, it returns a null pointer.
     /// The client must assure the following things:
     /// - `alignment` is greater than zero
@@ -14,15 +14,15 @@ extern "C" {
     /// The program may be forced to abort if the constrains are not full-filled.
     pub fn rust_alloc(alignment: size_t, size: size_t) -> *mut c_void;
 
-    /// De-allocate the memory at the given address with the given alignment and size. 
+    /// De-allocate the memory at the given address with the given alignment and size.
     /// The client must assure the following things:
     /// - the memory is acquired using the same allocator and the pointer points to the start position.
     /// - `alignment` and `size` is the same as allocation
     /// The program may be forced to abort if the constrains are not full-filled.
     pub fn rust_dealloc(ptr: *mut c_void, alignment: size_t, size: size_t) -> c_void;
 
-    /// Re-allocate the memory at the given address with the given alignment and size. 
-    /// On success, it returns a pointer pointing to the required memory address. 
+    /// Re-allocate the memory at the given address with the given alignment and size.
+    /// On success, it returns a pointer pointing to the required memory address.
     /// The memory content within the `new_size` will remains the same as previous.
     /// On failure, it returns a null pointer. In this situation, the previous memory is not returned to the allocator.
     /// The client must assure the following things:
@@ -30,13 +30,18 @@ extern "C" {
     /// - `alignment` and `old_size` is the same as allocation
     /// - `alignment` fulfills all the requirements as `rust_alloc`
     /// The program may be forced to abort if the constrains are not full-filled.
-    pub fn rust_realloc(ptr: *mut c_void, alignment: size_t, old_size: size_t, new_size: size_t) -> *mut c_void;
-    
+    pub fn rust_realloc(
+        ptr: *mut c_void,
+        alignment: size_t,
+        old_size: size_t,
+        new_size: size_t,
+    ) -> *mut c_void;
+
     /// Allocate `count` items of `size` length each.
     /// Returns `null` if `count * size` overflows or on out-of-memory.
     /// All items are initialized to zero.
     pub fn sn_calloc(count: usize, size: usize) -> *mut c_void;
-    
+
     /// Allocate `size` bytes.
     /// Returns pointer to the allocated memory or null if out of memory.
     /// Returns a unique pointer if called with `size` 0.
@@ -55,7 +60,7 @@ extern "C" {
     /// Free previously allocated memory.
     /// The pointer `p` must have been allocated before (or be null).
     pub fn sn_free(p: *mut c_void);
-	
+
     /// Return the available bytes in a memory block.
     pub fn sn_malloc_usable_size(p: *const c_void) -> usize;
 }
@@ -67,7 +72,10 @@ mod tests {
     #[test]
     fn it_frees_memory_malloc() {
         let ptr = unsafe { rust_alloc(8, 8) } as *mut u8;
-        unsafe {*ptr = 127; assert_eq!(*ptr, 127)};
+        unsafe {
+            *ptr = 127;
+            assert_eq!(*ptr, 127)
+        };
         unsafe { rust_dealloc(ptr as *mut c_void, 8, 8) };
     }
     #[test]
@@ -75,20 +83,23 @@ mod tests {
         let ptr = unsafe { sn_malloc(8) } as *mut u8;
         unsafe { sn_free(ptr as *mut c_void) };
     }
-	
+
     #[test]
     fn it_frees_memory_sn_realloc() {
         let ptr = unsafe { sn_malloc(8) } as *mut u8;
         let ptr = unsafe { sn_realloc(ptr as *mut c_void, 8) } as *mut u8;
         unsafe { sn_free(ptr as *mut c_void) };
     }
-    
+
     #[test]
     fn it_reallocs_correctly() {
         let mut ptr = unsafe { rust_alloc(8, 8) } as *mut u8;
-        unsafe {*ptr = 127; assert_eq!(*ptr, 127)};
+        unsafe {
+            *ptr = 127;
+            assert_eq!(*ptr, 127)
+        };
         ptr = unsafe { rust_realloc(ptr as *mut c_void, 8, 8, 16) } as *mut u8;
-        unsafe {assert_eq!(*ptr, 127)};
+        unsafe { assert_eq!(*ptr, 127) };
         unsafe { rust_dealloc(ptr as *mut c_void, 8, 16) };
     }
 
