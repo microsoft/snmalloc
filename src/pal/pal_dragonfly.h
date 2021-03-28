0377fd88d420e03197e5428a2b4e283682c18cd1
Matthew Parkinson <mattpark@microsoft.com>
2021-03-28 20:31:49 +0100

    Improve BSDs support.

--- src/pal/pal_dragonfly.h
+++ src/pal/pal_dragonfly.h
@@ -25,6 +25,19 @@ namespace snmalloc
      * to add new features that they should add any required feature flags.
      */
     static constexpr uint64_t pal_features = PALPOSIX::pal_features;
+
+    /**
+     * Source of Entropy
+     *
+     * DragonflyBSD does not have getentropy, so use getrandom instead.
+     */
+    static uint64_t get_entropy64()
+    {
+      uint64_t result;
+      if (getrandom(&result, sizeof(result), 0) != sizeof(result))
+        error("Failed to get system randomness");
+      return result;
+    }
   };
 } // namespace snmalloc
 #endif
