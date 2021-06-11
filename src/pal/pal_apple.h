bb9af79b0b94206239592eb9816828275ebebba8
Amari Robinson <arobinson.5000@yahoo.com>
2021-06-11 18:01:44 -0500

    Rewrite Apple PAL using native APIs

--- src/pal/pal_apple.h
+++ src/pal/pal_apple.h
@@ -1,11 +1,20 @@
 #pragma once
 
 #ifdef __APPLE__
-#  include "pal_bsd.h"
 
+#  include "../ds/address.h"
+
+#  include <Security/Security.h>
 #  include <errno.h>
+#  include <execinfo.h>
+#  include <mach/mach_init.h>
+#  include <mach/mach_vm.h>
+#  include <mach/vm_region.h>
 #  include <mach/vm_statistics.h>
+#  include <mach/vm_types.h>
 #  include <utility>
+
+extern "C" int puts(const char* str);
 
 namespace snmalloc
 {
@@ -13,80 +22,171 @@ namespace snmalloc
    * PAL implementation for Apple systems (macOS, iOS, watchOS, tvOS...).
    */
   template<int PALAnonID = PALAnonDefaultID>
-  class PALApple : public PALBSD<PALApple<>>
+  class PALApple
   {
   public:
     /**
      * The features exported by this PAL.
-     *
-     * Currently, these are identical to the generic BSD PAL.  This field is
-     * declared explicitly to remind anyone who modifies this class that they
-     * should add any required features.
      */
-    static constexpr uint64_t pal_features = PALBSD::pal_features;
+    static constexpr uint64_t pal_features =
+      AlignedAllocation | LazyCommit | Entropy;
 
-    /**
-     * Anonymous page tag ID.
-     *
-     * Darwin platform allows to gives an ID to anonymous pages via
-     * the VM_MAKE_TAG's macro, from 240 up to 255 are guaranteed
-     * to be free of usage, however eventually a lower could be taken
-     * (e.g. LLVM sanitizers has 99) so we can monitor their states
-     * via vmmap for instance. This value is provided to `mmap` as the file
-     * descriptor for the mapping.
-     */
-    static constexpr int anonymous_memory_fd = VM_MAKE_TAG(PALAnonID);
+    static constexpr size_t page_size = Aal::aal_name == ARM ? 0x4000 : 0x1000;
 
-    /**
-     * Note: The root's implementation works fine on Intel
-     * however mprotect/PROT_NONE fails on ARM
-     * especially since the 11.2 release (seems known issue
-     * spotted in various projects; might be a temporary fix).
-     */
-    template<bool page_aligned = false>
-    static void zero(void* p, size_t size) noexcept
+    static constexpr size_t minimum_alloc_size = page_size;
+
+    static void print_stack_trace()
     {
-      if constexpr (Aal::aal_name != ARM)
-        PALBSD::zero(p, size);
-      else
-        ::bzero(p, size);
+      constexpr int SIZE = 1024;
+      void* buffer[SIZE];
+      auto nptrs = backtrace(buffer, SIZE);
+      fflush(stdout);
+      backtrace_symbols_fd(buffer, nptrs, STDOUT_FILENO);
+      puts("");
+      fflush(stdout);
     }
 
-#  if defined(PLATFORM_IS_ARM)
     /**
-     * Overriding here to mark the page as reusable
-     * rolling it as much as necessary.
-     * As above, the x86 h/w worked alright without this change
-     * however now large allocations work better and more reliably
-     * with on ARM, not to mention better RSS number accuracy
-     * for tools based on task_info API.
+     * Report a fatal error and exit.
+     */
+    [[noreturn]] static void error(const char* const str) noexcept
+    {
+      puts(str);
+      print_stack_trace();
+      abort();
+    }
+
+    /**
+     * Notify platform that we will not be using these pages.
      */
     static void notify_not_using(void* p, size_t size) noexcept
     {
-      SNMALLOC_ASSERT(is_aligned_block<PALBSD::page_size>(p, size));
-#    ifdef USE_POSIX_COMMIT_CHECKS
-      memset(p, 0x5a, size);
-#    endif
-      while (madvise(p, size, MADV_FREE_REUSABLE) == -1 && errno == EAGAIN)
-        ;
+      SNMALLOC_ASSERT(is_aligned_block<page_size>(p, size));
+
+      mach_vm_behavior_set(
+        mach_task_self(),
+        reinterpret_cast<mach_vm_address_t>(p),
+        mach_vm_size_t(size),
+        VM_BEHAVIOR_REUSABLE);
     }
 
     /**
-     * same remark as above but we need to mark the page as REUSE
-     * first
+     * Notify platform that we will be using these pages.
      */
     template<ZeroMem zero_mem>
     static void notify_using(void* p, size_t size) noexcept
     {
       SNMALLOC_ASSERT(
-        is_aligned_block<PALBSD::page_size>(p, size) || (zero_mem == NoZero));
-      while (madvise(p, size, MADV_FREE_REUSE) == -1 && errno == EAGAIN)
-        ;
+        is_aligned_block<page_size>(p, size) || (zero_mem == NoZero));
+
+      mach_vm_behavior_set(
+        mach_task_self(),
+        reinterpret_cast<mach_vm_address_t>(p),
+        mach_vm_size_t(size),
+        VM_BEHAVIOR_REUSE);
 
       if constexpr (zero_mem == YesZero)
+      {
         zero<true>(p, size);
+      }
     }
-#  endif
+
+    /**
+     * OS specific function for zeroing memory.
+     */
+    template<bool page_aligned = false>
+    static void zero(void* p, size_t size) noexcept
+    {
+      if (page_aligned || is_aligned_block<page_size>(p, size))
+      {
+        SNMALLOC_ASSERT(is_aligned_block<page_size>(p, size));
+
+        // mask has least-significant bits set
+        mach_vm_offset_t mask = page_size - 1;
+
+        int flags =
+          VM_FLAGS_FIXED | VM_FLAGS_OVERWRITE | VM_MAKE_TAG(PALAnonID);
+
+        mach_vm_address_t addr = reinterpret_cast<mach_vm_address_t>(p);
+
+        kern_return_t kr = mach_vm_map(
+          mach_task_self(),
+          &addr,
+          size,
+          mask,
+          flags,
+          MEMORY_OBJECT_NULL,
+          0,
+          TRUE,
+          VM_PROT_READ | VM_PROT_WRITE,
+          VM_PROT_READ | VM_PROT_WRITE,
+          VM_INHERIT_COPY);
+
+        if (kr == KERN_SUCCESS)
+        {
+          return;
+        }
+      }
+
+      ::bzero(p, size);
+    }
+
+    template<bool committed>
+    static void* reserve_aligned(size_t size) noexcept
+    {
+      SNMALLOC_ASSERT(bits::is_pow2(size));
+      SNMALLOC_ASSERT(size >= minimum_alloc_size);
+
+      // mask has least-significant bits set
+      mach_vm_offset_t mask = size - 1;
+
+      int flags =
+        VM_FLAGS_ANYWHERE | VM_FLAGS_RANDOM_ADDR | VM_MAKE_TAG(PALAnonID);
+
+      // must be initialized to 0 or addr is interepreted as a lower-bound.
+      mach_vm_address_t addr = 0;
+
+      kern_return_t kr = mach_vm_map(
+        mach_task_self(),
+        &addr,
+        size,
+        mask,
+        flags,
+        MEMORY_OBJECT_NULL,
+        0,
+        TRUE,
+        VM_PROT_READ | VM_PROT_WRITE,
+        VM_PROT_READ | VM_PROT_WRITE,
+        VM_INHERIT_COPY);
+
+      if (kr != KERN_SUCCESS)
+      {
+        error("Failed to allocate memory\n");
+      }
+
+      return reinterpret_cast<void*>(addr);
+    }
+
+    /**
+     * Source of Entropy
+     *
+     * Apple platforms do not have a getentropy implementation, so use
+     * SecRandomCopyBytes instead.
+     */
+    static uint64_t get_entropy64()
+    {
+      uint64_t result;
+      if (
+        SecRandomCopyBytes(
+          kSecRandomDefault,
+          sizeof(result),
+          reinterpret_cast<void*>(&result)) != errSecSuccess)
+      {
+        error("Failed to get system randomness");
+      }
+
+      return result;
+    }
   };
 } // namespace snmalloc
 #endif
