63ebfd4847a6fdba738ca5e07b7069bbb81fda09
Matthew Parkinson <mattpark@microsoft.com>
2021-07-16 10:36:13 +0100

    Remove address space usage from Pagemap.
    The pagemap allocates it self directly either from
    
      * the original fixed address range it is supplied, and returns the
        remaining space after the pagemap is removed; or
      * directly allocated from the PAL without using the address space
        manager.
    
    This change in layering is required for the next commit, which imposes
    the invariant that the pagemap has been committed for all spaced managed
    by the address space manager.

--- src/backend/pagemap.h
+++ src/backend/pagemap.h
@@ -19,17 +19,30 @@ namespace snmalloc
   private:
     static constexpr size_t SHIFT = GRANULARITY_BITS;
 
-    // Before init is called will contain a single entry
-    // that is the default value.  This is needed so that
-    // various calls do not have to check for nullptr.
-    //   free(nullptr)
-    // and
-    //   malloc_usable_size(nullptr)
-    // do not require an allocation to have ocurred before
-    // they are called.
+    /**
+     * Before init is called will contain a single entry
+     * that is the default value.  This is needed so that
+     * various calls do not have to check for nullptr.
+     *   free(nullptr)
+     * and
+     *   malloc_usable_size(nullptr)
+     * do not require an allocation to have ocurred before
+     * they are called.
+     */
     inline static const T default_value{};
+
+    /**
+     * The representation of the page map.
+     *
+     * Initially a single element to ensure nullptr operations
+     * work.
+     */
     T* body{const_cast<T*>(&default_value)};
 
+    /**
+     * If `has_bounds` is set, then these should contain the
+     * bounds of the heap that is being managed by this pagemap.
+     */
     address_t base{0};
     size_t size{0};
 
@@ -51,37 +64,66 @@ namespace snmalloc
   public:
     constexpr FlatPagemap() = default;
 
-    template<typename ASM, bool has_bounds_ = has_bounds>
-    std::enable_if_t<has_bounds_> init(ASM* a, address_t b, size_t s)
+    /**
+     * Initialise the pagemap with bounds.
+     *
+     * Returns usable range after pagemap has been allocated
+     */
+    template<bool has_bounds_ = has_bounds>
+    std::enable_if_t<has_bounds_, std::pair<CapPtr<void, CBChunk>, size_t>>
+    init(CapPtr<void, CBChunk> b, size_t s)
     {
       static_assert(
         has_bounds_ == has_bounds, "Don't set SFINAE template parameter!");
 #ifdef SNMALLOC_TRACING
-      std::cout << "Pagemap.init " << (void*)b << " (" << s << ")" << std::endl;
+      std::cout << "Pagemap.init " << b.unsafe_ptr() << " (" << s << ")"
+                << std::endl;
 #endif
       SNMALLOC_ASSERT(s != 0);
+      // TODO take account of pagemap size in the calculation of how big it
+      // needs to be.
+
       // Align the start and end.  We won't store for the very ends as they
       // are not aligned to a chunk boundary.
-      base = bits::align_up(b, bits::one_at_bit(GRANULARITY_BITS));
-      auto end = bits::align_down(b + s, bits::one_at_bit(GRANULARITY_BITS));
-      size = end - base;
-      body = a->template reserve<false, false>(
-                bits::next_pow2((size >> SHIFT) * sizeof(T)))
-               .template as_static<T>()
-               .unsafe_ptr();
+      auto heap_base = pointer_align_up(b, bits::one_at_bit(GRANULARITY_BITS));
+      auto end = pointer_align_down(
+        pointer_offset(b, s), bits::one_at_bit(GRANULARITY_BITS));
+      size = pointer_diff(heap_base, end);
+
+      // Put pagemap at start of range.
+      // TODO CHERI capability bound here!
+      body = b.as_reinterpret<T>().unsafe_ptr();
+
+      // Advance by size of pagemap.
+      // TODO CHERI capability bound here!
+      heap_base = pointer_align_up(
+        pointer_offset(b, (size >> SHIFT) * sizeof(T)),
+        bits::one_at_bit(GRANULARITY_BITS));
+      base = address_cast(heap_base);
+      SNMALLOC_ASSERT(
+        base == bits::align_up(base, bits::one_at_bit(GRANULARITY_BITS)));
+      return {heap_base, pointer_diff(heap_base, end)};
     }
 
-    template<typename ASM, bool has_bounds_ = has_bounds>
-    std::enable_if_t<!has_bounds_> init(ASM* a)
+    /**
+     * Initialise the pagemap without bounds.
+     */
+    template<bool has_bounds_ = has_bounds>
+    std::enable_if_t<!has_bounds_> init()
     {
       static_assert(
         has_bounds_ == has_bounds, "Don't set SFINAE template parameter!");
       static constexpr size_t COVERED_BITS =
         bits::ADDRESS_BITS - GRANULARITY_BITS;
       static constexpr size_t ENTRIES = bits::one_at_bit(COVERED_BITS);
-      auto new_body = (a->template reserve<false, false>(ENTRIES * sizeof(T)))
-                        .template as_static<T>()
-                        .unsafe_ptr();
+
+      // TODO request additional space, and move to random offset.
+
+      // TODO wasting space if size2 bigger than needed.
+      auto [new_body_untyped, size2] =
+        Pal::reserve_at_least(ENTRIES * sizeof(T));
+
+      auto new_body = reinterpret_cast<T*>(new_body_untyped);
 
       // Ensure bottom page is committed
       commit_entry(&new_body[0]);
@@ -90,8 +132,6 @@ namespace snmalloc
       new_body[0] = body[0];
 
       body = new_body;
-      // TODO this is pretty sparse, should we ignore huge pages for it?
-      //     madvise(body, size, MADV_NOHUGEPAGE);
     }
 
     /**
