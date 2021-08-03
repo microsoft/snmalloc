df16e9cec909dab8747c3145a8054e536cb975d3
David Chisnall <David.Chisnall@microsoft.com>
2021-08-03 15:22:57 +0100

    Don't use CHAR_BIT, it needs an extra header with libstdc++ for a constant that's always 8.

--- src/ds/bits.h
+++ src/ds/bits.h
@@ -48,7 +48,7 @@ namespace snmalloc
     constexpr T one_at_bit(S shift)
     {
       static_assert(std::is_integral_v<T>, "Type must be integral");
-      SNMALLOC_ASSERT(sizeof(T) * CHAR_BIT > static_cast<size_t>(shift));
+      SNMALLOC_ASSERT(sizeof(T) * 8 > static_cast<size_t>(shift));
       return (static_cast<T>(1)) << shift;
     }
 
