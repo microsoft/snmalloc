75c73aa9eccd485b065b3a6f3b5246257c3a5654
SchrodingerZhu <i@zhuyi.fan>
2021-11-29 18:35:30 +0800

    adjust flag lock and comments
    Signed-off-by: SchrodingerZhu <i@zhuyi.fan>

--- src/ds/flaglock.h
+++ src/ds/flaglock.h
@@ -6,8 +6,17 @@
 
 namespace snmalloc
 {
+  /**
+   * @brief The DebugFlagWord struct
+   * Wrapper for std::atomic_flag so that we can examine
+   * the re-entrancy problem at debug mode.
+   */
   struct DebugFlagWord
   {
+    /**
+     * @brief flag
+     * The underlying atomic field.
+     */
     std::atomic_flag flag = ATOMIC_FLAG_INIT;
 
     constexpr DebugFlagWord() = default;
@@ -16,18 +25,30 @@ namespace snmalloc
     constexpr DebugFlagWord(Args&&... args) : flag(std::forward<Args>(args)...)
     {}
 
+    /**
+     * @brief set_owner
+     * Record the identity of the locker.
+     */
     void set_owner()
     {
       SNMALLOC_ASSERT(nullptr == owner);
       owner = get_thread_identity();
     }
 
+    /**
+     * @brief clear_owner
+     * Set the identity to null.
+     */
     void clear_owner()
     {
       SNMALLOC_ASSERT(get_thread_identity() == owner);
       owner = nullptr;
     }
 
+    /**
+     * @brief assert_not_owned_by_current_thread
+     * Assert the lock should not be held already by current thread.
+     */
     void assert_not_owned_by_current_thread()
     {
       SNMALLOC_ASSERT(get_thread_identity() != owner);
@@ -36,8 +57,18 @@ namespace snmalloc
   private:
     using ThreadIdentity = int const*;
 
+    /**
+     * @brief owner
+     * We use a pointer to TLS field as the thread identity.
+     * std::thread::id can be another solution but it does not
+     * support `constexpr` initialisation on some platforms.
+     */
     ThreadIdentity owner = nullptr;
 
+    /**
+     * @brief get_thread_identity
+     * @return The identity of current thread.
+     */
     inline ThreadIdentity get_thread_identity()
     {
       static thread_local int SNMALLOC_THREAD_IDENTITY = 0;
@@ -45,6 +76,12 @@ namespace snmalloc
     }
   };
 
+  /**
+   * @brief The ReleaseFlagWord struct
+   * The shares the same structure with DebugFlagWord but
+   * all member functions associated with ownership checkings
+   * are empty so that they can be optimised out at Release mode.
+   */
   struct ReleaseFlagWord
   {
     std::atomic_flag flag = ATOMIC_FLAG_INIT;
@@ -75,11 +112,31 @@ namespace snmalloc
   public:
     FlagLock(FlagWord& lock) : lock(lock)
     {
+#ifdef __cpp_lib_atomic_flag_test
+      do
+      {
+        // test once with acquire ordering at the beginning
+        if (!lock.flag.test_and_set(std::memory_order_acquire))
+        {
+          break;
+        }
+        // assert_not_owned_by_current_thread is only called when the first
+        // acquiring is failed; which means the lock is already held somewhere
+        // else.
+        lock.assert_not_owned_by_current_thread();
+        // only need to do relaxed ordering in the busy spin loop
+        while (lock.flag.test(std::memory_order_relaxed))
+        {
+          Aal::pause();
+        }
+      } while (true);
+#else
       while (lock.flag.test_and_set(std::memory_order_acquire))
       {
         lock.assert_not_owned_by_current_thread();
         Aal::pause();
       }
+#endif
       lock.set_owner();
     }
 
