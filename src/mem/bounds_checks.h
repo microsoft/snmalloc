93efbb4807acf9e3d3995f891670582a152242a3
David Chisnall <davidchisnall@users.noreply.github.com>
2022-02-25 09:59:51 +0000

    Refactor error reporting for bounds checks. (#464)
    This introduces a very limited formatter that can embed strings and hex
    representations of pointers / integers in an internal buffer. This is
    used to format error strings for passing to `Pal::error`.  This is used,
    in turn, by a wrapper for reporting bounds checks, which can be used by
    external functions to implement bounds checks.
    
    This removes the sprintf_l usage from the bounds checks.
    
    This provides enough of a format implementation that the tests
    introduced in #465 can be refactored to use this, instead of their
    custom `printf` wrapper and that can be used by SNMALLOC_CHECK.  This
    will be a follow-on PR.

---
+++ src/mem/bounds_checks.h
@@ -0,0 +1,108 @@
+#pragma once
+#include "../snmalloc.h"
+
+namespace snmalloc
+{
+  /**
+   * Should we check loads?  This defaults to on in debug builds, off in
+   * release (store-only checks) and can be overridden by defining the macro
+   * `SNMALLOC_CHECK_LOADS` to true or false.
+   */
+  static constexpr bool CheckReads =
+#ifdef SNMALLOC_CHECK_LOADS
+    SNMALLOC_CHECK_LOADS
+#else
+    DEBUG
+#endif
+    ;
+
+  /**
+   * Should we fail fast when we encounter an error?  With this set to true, we
+   * just issue a trap instruction and crash the process once we detect an
+   * error. With it set to false we print a helpful error message and then crash
+   * the process.  The process may be in an undefined state by the time the
+   * check fails, so there are potentially security implications to turning this
+   * off. It defaults to true for debug builds, false for release builds and
+   * can be overridden by defining the macro `SNMALLOC_FAIL_FAST` to true or
+   * false.
+   */
+  static constexpr bool FailFast =
+#ifdef SNMALLOC_FAIL_FAST
+    SNMALLOC_FAIL_FAST
+#else
+    !DEBUG
+#endif
+    ;
+
+  /**
+   * Report an error message for a failed bounds check and then abort the
+   * program.
+   * `p` is the input pointer and `len` is the offset from this pointer of the
+   * bounds.  `msg` is the message that will be reported along with the
+   * start and end of the real object's bounds.
+   */
+  SNMALLOC_SLOW_PATH SNMALLOC_UNUSED_FUNCTION inline void
+    report_fatal_bounds_error [[noreturn]] (
+      void* p, size_t len, const char* msg, decltype(ThreadAlloc::get())& alloc)
+  {
+    report_fatal_error(
+      "{}: {} is in allocation {}--{}, offset {} is past the end\n",
+      msg,
+      p,
+      alloc.template external_pointer<Start>(p),
+      alloc.template external_pointer<OnePastEnd>(p),
+      len);
+  }
+
+  /**
+   * The direction for a bounds check.
+   */
+  enum class CheckDirection
+  {
+    /**
+     * A read bounds check, performed only when read checks are enabled.
+     */
+    Read,
+
+    /**
+     * A write bounds check, performed unconditionally.
+     */
+    Write
+  };
+
+  /**
+   * Check whether a pointer + length is in the same object as the pointer.
+   * Fail with the error message from the third argument if not.
+   *
+   * The template parameter indicates whether this is a read.  If so, this
+   * function is a no-op when `CheckReads` is false.
+   */
+  template<CheckDirection Direction = CheckDirection::Write>
+  SNMALLOC_FAST_PATH_INLINE void
+  check_bounds(const void* ptr, size_t len, const char* msg = "")
+  {
+    if constexpr ((Direction == CheckDirection::Write) || CheckReads)
+    {
+      auto& alloc = ThreadAlloc::get();
+      void* p = const_cast<void*>(ptr);
+
+      if (SNMALLOC_UNLIKELY(!alloc.check_bounds(ptr, len)))
+      {
+        if constexpr (FailFast)
+        {
+          UNUSED(p, len, msg);
+          SNMALLOC_FAST_FAIL();
+        }
+        else
+        {
+          report_fatal_bounds_error(p, len, msg, alloc);
+        }
+      }
+    }
+    else
+    {
+      UNUSED(ptr, len, msg);
+    }
+  }
+
+}
