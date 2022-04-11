f0361f8c0153a11fb91e86ede3b0f76d13e65f4f
Robert Norton <rmn30@cam.ac.uk>
2022-04-11 13:29:06 +0100

    Pagemap move (#504)
    * Move PageMap interface into pagemap.h and rename to BasicPagemap.
    
    Refactoring suggested by David. This allows custom backends to reuse
    or extend the BasicPagemap. It has template parameters for the PAL,
    concrete page map and page map entry types as well as the Backend (so
    that it can be friends).  BackendAllocator provides an exmple page map
    entry type.

--- src/snmalloc/backend_helpers/pagemap.h
+++ src/snmalloc/backend_helpers/pagemap.h
@@ -325,4 +325,105 @@ namespace snmalloc
       body[p >> SHIFT] = t;
     }
   };
+
+  /**
+   * This is a generic implementation of the backend's interface to the page
+   * map. It takes a concrete page map implementation (probably FlatPageMap
+   * above) and entry type. It is friends with the backend passed in as a
+   * template parameter so that the backend can initialise the concrete page map
+   * and use set_metaentry which no one else should use.
+   */
+  template<
+    typename Backend,
+    typename PAL,
+    typename ConcreteMap,
+    typename PageMapEntry,
+    bool fixed_range>
+  class BasicPagemap
+  {
+  public:
+    /**
+     * Export the type stored in the pagemap.
+     */
+    using Entry = PageMapEntry;
+
+  private:
+    friend Backend;
+
+    /**
+     * Instance of the concrete pagemap, accessible to the backend so that
+     * it can call the init method whose type dependent on fixed_range.
+     */
+    SNMALLOC_REQUIRE_CONSTINIT
+    static inline ConcreteMap concretePagemap;
+
+    /**
+     * Set the metadata associated with a chunk.
+     */
+    SNMALLOC_FAST_PATH
+    static void set_metaentry(address_t p, size_t size, const Entry& t)
+    {
+      for (address_t a = p; a < p + size; a += MIN_CHUNK_SIZE)
+      {
+        concretePagemap.set(a, t);
+      }
+    }
+
+  public:
+    /**
+     * Get the metadata associated with a chunk.
+     *
+     * Set template parameter to true if it not an error
+     * to access a location that is not backed by a chunk.
+     */
+    template<bool potentially_out_of_range = false>
+    SNMALLOC_FAST_PATH static const auto& get_metaentry(address_t p)
+    {
+      return concretePagemap.template get<potentially_out_of_range>(p);
+    }
+
+    /**
+     * Get the metadata associated with a chunk.
+     *
+     * Set template parameter to true if it not an error
+     * to access a location that is not backed by a chunk.
+     */
+    template<bool potentially_out_of_range = false>
+    SNMALLOC_FAST_PATH static auto& get_metaentry_mut(address_t p)
+    {
+      return concretePagemap.template get_mut<potentially_out_of_range>(p);
+    }
+
+    /**
+     * Register a range in the pagemap as in-use, requiring it to allow writing
+     * to the underlying memory.
+     */
+    static void register_range(address_t p, size_t sz)
+    {
+      concretePagemap.register_range(p, sz);
+    }
+
+    /**
+     * Return the bounds of the memory this back-end manages as a pair of
+     * addresses (start then end).  This is available iff this is a
+     * fixed-range Backend.
+     */
+    template<bool fixed_range_ = fixed_range>
+    static SNMALLOC_FAST_PATH
+      std::enable_if_t<fixed_range_, std::pair<address_t, address_t>>
+      get_bounds()
+    {
+      static_assert(fixed_range_ == fixed_range, "Don't set SFINAE parameter!");
+
+      return concretePagemap.get_bounds();
+    }
+
+    /**
+     * Return whether the pagemap is initialised, ready for access.
+     */
+    static bool is_initialised()
+    {
+      return concretePagemap.is_initialised();
+    }
+  };
 } // namespace snmalloc
