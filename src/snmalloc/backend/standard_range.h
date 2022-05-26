26ed69d097c5ef135798f664e03037e8bf9e4cfd
Matthew Parkinson <mattpark@microsoft.com>
2022-05-26 09:07:40 +0100

    Add some comments on range structure

--- src/snmalloc/backend/standard_range.h
+++ src/snmalloc/backend/standard_range.h
@@ -6,6 +6,18 @@
 
 namespace snmalloc
 {
+  /**
+   * Default configuration that does not provide any meta-data protection.
+   * 
+   * PAL is the underlying PAL that is used to Commit memory ranges.
+   * 
+   * Base is where memory is sourced from.
+   * 
+   * MinSizeBits is the minimum request size that can be passed to Base.
+   * On Windows this 16 as VirtualAlloc cannot reserve less than 64KiB.
+   * Alternative configurations might make this 2MiB so that huge pages
+   * can be used.
+   */
   template<
     typename PAL,
     typename Pagemap,
@@ -33,10 +45,13 @@ namespace snmalloc
     // Expose a global range for the initial allocation of meta-data.
     using GlobalMetaRange = Pipe<ObjectRange, GlobalRange<>>;
 
+    // Where we get user allocations from.
     ObjectRange object_range;
 
+    // Where we get meta-data allocations from.
     ObjectRange& get_meta_range()
     {
+      // Use the object range to service meta-data requests.
       return object_range;
     }
   };
