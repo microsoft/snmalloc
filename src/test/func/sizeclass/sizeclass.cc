56d067f0caff149a6b9a5a6bcfb7ab74b6bc8835
Nathaniel Wesley Filardo <nfilardo@microsoft.com>
2024-01-04 20:20:29 +0000

    test/sizeclass: handle nonzero minimum sizeclasses

--- src/test/func/sizeclass/sizeclass.cc
+++ src/test/func/sizeclass/sizeclass.cc
@@ -8,6 +8,9 @@ snmalloc::smallsizeclass_t size_to_sizeclass(size_t size)
   return snmalloc::size_to_sizeclass(size);
 }
 
+static constexpr snmalloc::smallsizeclass_t minimum_sizeclass =
+  snmalloc::size_to_sizeclass_const(snmalloc::MIN_ALLOC_SIZE);
+
 void test_align_size()
 {
   bool failed = false;
@@ -86,12 +89,14 @@ int main(int, char**)
       slab_size != snmalloc::sizeclass_to_slab_size(sz))
     {
       slab_size = snmalloc::sizeclass_to_slab_size(sz);
-      std::cout << std::endl;
+      std::cout << std::endl << "slab size: " << slab_size << std::endl;
     }
 
     size_t size = snmalloc::sizeclass_to_size(sz);
     std::cout << (size_t)sz << " |-> "
-              << "[" << size_low + 1 << ", " << size << "]" << std::endl;
+              << "[" << size_low + 1 << ", " << size << "]"
+              << (sz == minimum_sizeclass ? " is minimum class" : "")
+              << std::endl;
 
     if (size < size_low)
     {
@@ -102,18 +107,34 @@ int main(int, char**)
 
     for (size_t i = size_low + 1; i <= size; i++)
     {
-      if (size_to_sizeclass(i) != sz)
-      {
-        std::cout << "Size " << i << " has sizeclass "
-                  << (size_t)size_to_sizeclass(i) << " but expected sizeclass "
-                  << (size_t)sz << std::endl;
-        failed = true;
-      }
+      /* All sizes should, via bit-math, come back to their class value */
       if (snmalloc::size_to_sizeclass_const(i) != sz)
       {
         std::cout << "Size " << i << " has _const sizeclass "
                   << (size_t)snmalloc::size_to_sizeclass_const(i)
                   << " but expected sizeclass " << (size_t)sz << std::endl;
+        failed = true;
+      }
+
+      if (size < snmalloc::MIN_ALLOC_SIZE)
+      {
+        /*
+         * It is expected that these sizes have the "wrong" class from tabular
+         * lookup: they will have been clipped up to the minimum class.
+         */
+        if (size_to_sizeclass(i) != minimum_sizeclass)
+        {
+          std::cout << "Size " << i << " below minimum size; sizeclass "
+                    << (size_t)size_to_sizeclass(i) << " not expected minimum "
+                    << (size_t)minimum_sizeclass << std::endl;
+          failed = true;
+        }
+      }
+      else if (size_to_sizeclass(i) != sz)
+      {
+        std::cout << "Size " << i << " has sizeclass "
+                  << (size_t)size_to_sizeclass(i) << " but expected sizeclass "
+                  << (size_t)sz << std::endl;
         failed = true;
       }
     }
