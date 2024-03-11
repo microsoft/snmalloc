5f1f4fb4b335ddfcb91b6b1b8d8f4bf678786996
Nathaniel Wesley Filardo <nfilardo@microsoft.com>
2024-03-11 22:26:07 +0000

    RemoteCache associative cache of rings

--- src/snmalloc/mem/remotecache.h
+++ src/snmalloc/mem/remotecache.h
@@ -31,26 +31,35 @@ namespace snmalloc
   template<typename Config>
   class RemoteDeallocCacheBatching
   {
-    freelist::Builder<false, true> open_builder;
-    typename Config::PagemapEntry::SlabMetadata* open_meta = nullptr;
-    RemoteAllocator::alloc_id_t open_target = 0;
+    static constexpr size_t RING_ASSOC = 2;
+
+    std::array<freelist::Builder<false, true>, RING_ASSOC> open_builder;
+    std::array<typename Config::PagemapEntry::SlabMetadata*, RING_ASSOC>
+      open_meta = {nullptr};
+    std::array<RemoteAllocator::alloc_id_t, RING_ASSOC> open_target = {0};
 
     template<typename Forward>
-    SNMALLOC_FAST_PATH void close_pending(Forward forward)
+    SNMALLOC_FAST_PATH void close_one_pending(Forward forward, size_t ix)
     {
       auto rmsg = RemoteMessage::mk_from_freelist_builder(
-        open_builder, freelist::Object::key_root, open_meta->as_key_tweak());
-      forward(open_target, rmsg);
+        open_builder[ix],
+        freelist::Object::key_root,
+        open_meta[ix]->as_key_tweak());
+
+      forward(open_target[ix], rmsg);
+
+      open_meta[ix] = nullptr;
     }
 
-    SNMALLOC_FAST_PATH void init_pending(
+    SNMALLOC_FAST_PATH void init_one_pending(
+      size_t ix,
       typename Config::PagemapEntry::SlabMetadata* meta,
       RemoteAllocator::alloc_id_t id)
     {
-      open_builder.init(
-        0, freelist::Object::key_root, open_meta->as_key_tweak());
-      open_meta = meta;
-      open_target = id;
+      open_builder[ix].init(
+        0, freelist::Object::key_root, open_meta[ix]->as_key_tweak());
+      open_meta[ix] = meta;
+      open_target[ix] = id;
     }
 
   public:
@@ -61,31 +70,62 @@ namespace snmalloc
       freelist::HeadPtr r,
       Forward forward)
     {
-      if (meta != open_meta)
+      for (size_t ix = 0; ix < RING_ASSOC; ix++)
       {
-        if (open_meta != nullptr)
+        if (meta == open_meta[ix])
         {
-          close_pending(forward);
+          open_builder[ix].add(
+            r, freelist::Object::key_root, meta->as_key_tweak());
+          return;
         }
-        init_pending(meta, target_id);
       }
 
-      open_builder.add(r, freelist::Object::key_root, meta->as_key_tweak());
+      // No hit in cache, so find an available or victim line.
+
+      size_t victim_ix = 0;
+      size_t victim_size = 0;
+      for (size_t ix = 0; ix < RING_ASSOC; ix++)
+      {
+        if (open_meta[ix] == nullptr)
+        {
+          victim_ix = ix;
+          break;
+        }
+
+        size_t szix = open_builder[ix].extract_segment_length();
+        if (szix > victim_size)
+        {
+          victim_size = szix;
+          victim_ix = ix;
+        }
+      }
+
+      if (open_meta[victim_ix] != nullptr)
+      {
+        close_one_pending(forward, victim_ix);
+      }
+      init_one_pending(victim_ix, meta, target_id);
+
+      open_builder[victim_ix].add(
+        r, freelist::Object::key_root, meta->as_key_tweak());
     }
 
     template<typename Forward>
     SNMALLOC_FAST_PATH void close_all(Forward forward)
     {
-      if (open_meta != nullptr)
+      for (size_t ix = 0; ix < RING_ASSOC; ix++)
       {
-        close_pending(forward);
-        open_meta = nullptr;
+        if (open_meta[ix] != nullptr)
+        {
+          close_one_pending(forward, ix);
+          open_meta[ix] = nullptr;
+        }
       }
     }
 
     void init()
     {
-      open_meta = nullptr;
+      open_meta = {nullptr};
     }
   };
 
