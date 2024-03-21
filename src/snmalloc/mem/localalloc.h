79dddadabf04014d118a5e66676ef856485fa7db
Nathaniel Wesley Filardo <nfilardo@microsoft.com>
2024-03-21 21:20:03 +0000

    Try: remotecache: don't store allocator IDs
    We can, as Matt so kindly reminds me, go get them from the pagemap.  Since we
    need this value only when closing a ring, the read from over there is probably
    not very onerous.  (We could also get the slab pointer from an object in the
    ring, but we need that whenever inserting into the cache, so it's probably more
    sensible to store that locally?)

--- src/snmalloc/mem/localalloc.h
+++ src/snmalloc/mem/localalloc.h
@@ -286,7 +286,7 @@ namespace snmalloc
           address_cast(entry.get_slab_metadata()));
 #endif
         local_cache.remote_dealloc_cache.template dealloc<sizeof(CoreAlloc)>(
-          entry.get_slab_metadata(), entry.get_remote()->trunc_id(), p);
+          entry.get_slab_metadata(), p);
         post_remote_cache();
         return;
       }
@@ -679,7 +679,7 @@ namespace snmalloc
         if (local_cache.remote_dealloc_cache.reserve_space(entry))
         {
           local_cache.remote_dealloc_cache.template dealloc<sizeof(CoreAlloc)>(
-            entry.get_slab_metadata(), remote->trunc_id(), p_tame);
+            entry.get_slab_metadata(), p_tame);
 #  ifdef SNMALLOC_TRACING
           message<1024>(
             "Remote dealloc fast {} ({}, {})",
