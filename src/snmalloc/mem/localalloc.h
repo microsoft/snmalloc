1ae868771db3bbb2cacc3ab02f8d11146d3a5b00
Nathaniel Wesley Filardo <nfilardo@microsoft.com>
2024-03-21 20:15:18 +0000

    Matt's fix for slow-path codegen

--- src/snmalloc/mem/localalloc.h
+++ src/snmalloc/mem/localalloc.h
@@ -658,6 +658,12 @@ namespace snmalloc
         return;
       }
 
+      dealloc_remote(entry, p_tame);
+    }
+
+    SNMALLOC_SLOW_PATH void
+    dealloc_remote(const PagemapEntry& entry, capptr::Alloc<void> p_tame)
+    {
       RemoteAllocator* remote = entry.get_remote();
       if (SNMALLOC_LIKELY(remote != nullptr))
       {
@@ -677,8 +683,8 @@ namespace snmalloc
 #  ifdef SNMALLOC_TRACING
           message<1024>(
             "Remote dealloc fast {} ({}, {})",
-            p_raw,
-            alloc_size(p_raw),
+            address_cast(p_tame),
+            alloc_size(p_tame.unsafe_ptr()),
             address_cast(entry.get_slab_metadata()));
 #  endif
           return;
