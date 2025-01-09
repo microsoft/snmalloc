ca691b21a95dde0746ca1c11b12497dc21d56fb0
schrodingerzhu <i@zhuyi.fan>
2025-01-09 20:20:03 +0800

    fix build

--- src/snmalloc/stl/cxx/array.h
+++ src/snmalloc/stl/cxx/array.h
@@ -4,12 +4,12 @@
 
 namespace snmalloc
 {
-  namespace proxy
+  namespace stl
   {
     template<typename T, size_t N>
     using Array = std::array<T, N>;
 
     using std::begin;
     using std::end;
-  } // namespace proxy
+  } // namespace stl
 } // namespace snmalloc
