ed459fd4a7cd26e70f15ab0a900982a86fbccd27
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2025-03-26 17:58:51 -0400

    revert

--- src/test/func/multi_atexit/multi_atexit.cc
+++ src/test/func/multi_atexit/multi_atexit.cc
@@ -1,15 +1,21 @@
-#ifdef __linux__
+#if defined(__linux__) && !__has_feature(address_sanitizer)
 #  include <snmalloc/snmalloc.h>
 #  include <stdlib.h>
 
 void do_nothing() {}
-#define SNMALLOC_NAME_MANGLE(X) X
-#include "snmalloc/override/malloc.cc"
+extern "C" void * calloc(size_t num, size_t size) {
+  return snmalloc::libc::calloc(num, size);
+}
+
+extern "C" void free(void * p) {
+  if (snmalloc::is_owned(p))
+    return snmalloc::libc::free(p);
+}
 
 #endif
 
 int main() {
-#ifdef __linux__
+#if defined(__linux__) && !__has_feature(address_sanitizer)
   for (int i = 0; i < 8192; ++i)
     atexit(do_nothing);
 #endif
