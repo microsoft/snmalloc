11a11830a04c457707eef85e852294733a568dd3
Matthew Parkinson <mattpark@microsoft.com>
2025-03-27 16:42:37 +0000

    Add test for reentrancy of C++ destructors and allocation

---
+++ src/test/func/multi_threadatexit/multi_threadatexit.cc
@@ -0,0 +1,55 @@
+#include <thread>
+
+#ifndef __has_feature
+#  define __has_feature(x) 0
+#endif
+
+#if defined(__linux__) && !__has_feature(address_sanitizer) && \
+  !defined(__SANITIZE_ADDRESS__)
+#  define RUN_TEST
+#endif
+
+#ifdef RUN_TEST
+#  include <snmalloc/snmalloc.h>
+#  include <stdlib.h>
+
+template<size_t N, size_t M = 0>
+void thread_destruct()
+{
+  snmalloc::message("thread_destruct<{}, {}> start", N, M);
+  static thread_local snmalloc::OnDestruct destruct{[](){
+    snmalloc::message("thread_destruct<{}, {}> destructor", N, M);
+  }};
+  snmalloc::message("thread_destruct<{}, {}> end", N, M);
+
+  if constexpr (N > M + 1)
+  {
+    // destructor
+    thread_destruct<N, (M + N)/2>();
+    thread_destruct<(M + N)/2, M>();
+  }
+}
+
+// We only selectively override these functions. Otherwise, malloc may be called
+// before atexit triggers the first initialization attempt.
+
+extern "C" void* calloc(size_t num, size_t size)
+{
+  snmalloc::message("calloc({}, {})", num, size);
+  return snmalloc::libc::calloc(num, size);
+}
+
+extern "C" void free(void* p)
+{
+  snmalloc::message("free({})", p);
+  if (snmalloc::is_owned(p))
+    return snmalloc::libc::free(p);
+  // otherwise, just leak the memory
+}
+
+#endif
+
+int main()
+{
+  std::thread(thread_destruct<1000>).join();
+}
