a14a017b655f9ba7c80df4042f540331be564cd9
Matthew Parkinson <mjp41@users.noreply.github.com>
2026-02-26 15:46:26 +0000

    Fix performance issue in Red Black Tree init (#818)
    * Benchmark for repeated large deallocations.
    
    * Remove unnecessary init from fast path
    
    * Clangformat

--- src/snmalloc/ds_core/redblacktree.h
+++ src/snmalloc/ds_core/redblacktree.h
@@ -101,7 +101,7 @@ namespace snmalloc
       H ptr;
 
     public:
-      ChildRef() : ptr(nullptr) {}
+      constexpr ChildRef() = default;
 
       ChildRef(H p) : ptr(p) {}
 
@@ -232,7 +232,15 @@ namespace snmalloc
     struct RBStep
     {
       ChildRef node;
-      bool dir = false;
+      bool dir;
+
+      // Default constructor needed for Array<RBStep, 128>.
+      constexpr RBStep() = default;
+
+      // Remove copy constructors to avoid accidentally copying and mutating the
+      // path.
+      RBStep(const RBStep& other) = delete;
+      RBStep& operator=(const RBStep& other) = delete;
 
       /**
        * Update the step to point to a new node and direction.
