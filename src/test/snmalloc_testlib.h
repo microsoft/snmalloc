c56f892db32b70d389b51a84e84d09744e6bbf5b
Matthew Parkinson <mattpark@microsoft.com>
2026-03-27 18:25:12 +0000

    Convert batch 9a tests to use testlib
    Convert simple alloc/dealloc tests to use snmalloc_testlib.h instead
    of snmalloc.h:
    - func/first_operation
    - func/teardown
    - func/multi_atexit
    - func/multi_threadatexit
    
    Replace MAX_SMALL_SIZECLASS_BITS with max_small_sizeclass_bits().
    Replace alloc<Zero> with alloc<ZeroMem::YesZero>.
    Add pal/pal.h include to testlib header for report_fatal_error.

--- src/test/snmalloc_testlib.h
+++ src/test/snmalloc_testlib.h
@@ -17,7 +17,7 @@
 #include <snmalloc/ds_core/defines.h>
 #include <snmalloc/ds_core/helpers.h>
 #include <snmalloc/ds_core/sizeclassstatic.h>
-#include <snmalloc/pal/pal_consts.h>
+#include <snmalloc/pal/pal.h>
 #include <stddef.h>
 #include <stdint.h>
 
