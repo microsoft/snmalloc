3afe15fb84d7870aa671902a5dc50042c0487f22
Paul Heidekrüger <paul@paulhdk.com>
2026-03-29 15:36:14 +0200

    Test deletion of null pointers

--- src/test/func/new/new.cc
+++ src/test/func/new/new.cc
@@ -6,12 +6,30 @@
 
 using namespace snmalloc;
 
+void test_delete_null()
+{
+  operator delete(nullptr, 42);
+  operator delete(nullptr, std::nothrow);
+  operator delete(nullptr, 42, std::align_val_t{OS_PAGE_SIZE});
+  operator delete(nullptr, std::align_val_t{OS_PAGE_SIZE});
+  operator delete(nullptr, std::align_val_t{OS_PAGE_SIZE}, std::nothrow);
+  operator delete[](nullptr);
+  operator delete[](nullptr, 42);
+  operator delete[](nullptr, std::nothrow);
+  operator delete[](nullptr, std::align_val_t{OS_PAGE_SIZE});
+  operator delete[](nullptr, 42, std::align_val_t{OS_PAGE_SIZE});
+  operator delete[](nullptr, std::align_val_t{OS_PAGE_SIZE}, std::nothrow);
+}
+
 int main(int argc, char** argv)
 {
   UNUSED(argc);
   UNUSED(argv);
 
   setup();
+
+  START_TEST("Test delete / delete[] nullptr");
+  test_delete_null();
 
   snmalloc::debug_check_empty();
   return 0;
