89b236173843a6834deb7b53061e59d6365541d8
Matthew Parkinson <mattpark@microsoft.com>
2026-03-30 17:51:08 +0100

    Remove DefaultPal dependency from test/helpers.h
    Replace DefaultPal::message() calls in START_TEST and INFO macros
    with snmalloc::message() which is now defined in ds_core/helpers.h.
    This removes the PAL dependency from test infrastructure, allowing
    tests that only need ds_core/ or ds_aal/ headers to avoid including
    pal/pal.h.

--- src/test/helpers.h
+++ src/test/helpers.h
@@ -18,8 +18,7 @@ namespace snmalloc
   do \
   { \
     current_test = __PRETTY_FUNCTION__; \
-    MessageBuilder<1024> mb{"Starting test: " msg "\n", ##__VA_ARGS__}; \
-    DefaultPal::message(mb.get_message()); \
+    snmalloc::message<1024>("Starting test: " msg, ##__VA_ARGS__); \
   } while (0)
 
   /**
@@ -32,8 +31,7 @@ namespace snmalloc
 #define INFO(msg, ...) \
   do \
   { \
-    MessageBuilder<1024> mb{msg "\n", ##__VA_ARGS__}; \
-    DefaultPal::message(mb.get_message()); \
+    snmalloc::message<1024>(msg, ##__VA_ARGS__); \
   } while (0)
 
 }
