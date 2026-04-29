9dd722df821a93240f31740ee9ea1259b2669968
Paul Heidekrüger <paul@paulhdk.com>
2026-04-29 15:11:23 +0200

    Revert back to function pointers instead of lambdas
    Avoid having to hard-code a suppression of certain GCC warnings

--- src/test/func/new/new.cc
+++ src/test/func/new/new.cc
@@ -11,13 +11,6 @@ int main()
 #  include <test/helpers.h>
 #  include <test/setup.h>
 
-// snmalloc implements operator new[] / operator delete[] identically to their
-// non-array counterparts, which causes GCC to produce false-positive
-// -Wmismatched-new-delete warnings when it inlines through lambdas.
-#  if defined(__GNUC__) && !defined(__clang__)
-#    pragma GCC diagnostic ignored "-Wmismatched-new-delete"
-#  endif
-
 using namespace snmalloc;
 constexpr std::array<size_t, 11> align_val_sizes = {
   8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, OS_PAGE_SIZE};
@@ -37,27 +30,27 @@ void test_delete_null()
   operator delete[](nullptr, std::align_val_t{OS_PAGE_SIZE}, std::nothrow);
 }
 
-template<typename New_Func, typename Del_Func>
-void test_zero_alloc(New_Func new_func, Del_Func del_func)
+template<typename New_Func>
+void test_zero_alloc(New_Func new_func)
 {
   void* non_zero = new_func(0);
   EXPECT(non_zero, "allocation with size '0' did not return a valid pointer");
-  del_func(non_zero);
+  operator delete(non_zero);
 }
 
 // SNMALLOC_EXPORT void* operator new(size_t size)
 // SNMALLOC_EXPORT void* operator new[](size_t size)
 // SNMALLOC_EXPORT void operator delete(void* p) EXCEPTSPEC
 // SNMALLOC_EXPORT void* operator delete[](void* p) EXCEPTSPEC
-template<typename New_Func, typename Del_Func>
-void test_new_delete_simple(New_Func new_fun, Del_Func del_fun, size_t size)
+template<void* (*new_fun)(size_t), void (*del_fun)(void*) EXCEPTSPEC>
+void test_new_delete_simple(size_t size)
 {
   void* non_zero = new_fun(size);
   bool caught_bad_alloc = false;
   EXPECT(non_zero, "expected valid address, but instead got {}", non_zero);
   del_fun(non_zero);
 
-  test_zero_alloc(new_fun, del_fun);
+  test_zero_alloc(new_fun);
 
   try
   {
@@ -82,22 +75,25 @@ void test_new_delete_simple(New_Func new_fun, Del_Func del_fun, size_t size)
 // noexcept
 // SNMALLOC_EXPORT void* operator delete[](size_t size, const std::nothrow_t&)
 // noexcept
-template<typename New_Func, typename Del_Func>
-void test_new_delete_nothrow(New_Func new_fun, Del_Func del_fun)
+template<
+  void* (*new_fun)(size_t, const std::nothrow_t&),
+  void (*del_fun)(void*, const std::nothrow_t&) EXCEPTSPEC>
+void test_new_delete_nothrow()
 {
-  void* impossible_alloc_unaligned = new_fun(static_cast<size_t>(-1));
+  void* impossible_alloc_unaligned =
+    new_fun(static_cast<size_t>(-1), std::nothrow);
   EXPECT(
     impossible_alloc_unaligned == nullptr,
     "Impossible allocation should not succeed");
-  del_fun(impossible_alloc_unaligned);
+  del_fun(impossible_alloc_unaligned, std::nothrow);
 
-  test_zero_alloc(new_fun, del_fun);
+  test_zero_alloc([](size_t s) { return new_fun(s, std::nothrow); });
 }
 
 // SNMALLOC_EXPORT void operator delete(void* p, size_t size) EXCEPTSPEC
 // SNMALLOC_EXPORT void operator delete[](void* p, size_t size) EXCEPTSPEC
-template<typename New_Func, typename Del_Func>
-void test_delete_size(New_Func new_fun, Del_Func del_fun, size_t size)
+template<void* (*new_fun)(size_t), void (*del_fun)(void*, size_t) EXCEPTSPEC>
+void test_delete_size(size_t size)
 {
   void* non_zero = new_fun(size);
   EXPECT(non_zero, "expected valid address, but instead got {}", non_zero);
@@ -108,8 +104,10 @@ void test_delete_size(New_Func new_fun, Del_Func del_fun, size_t size)
 // SNMALLOC_EXPORT void* operator new[](size_t size, std::align_val_t val)
 // SNMALLOC_EXPORT void operator delete(void* p, std::align_val_t) EXCEPTSPEC
 // SNMALLOC_EXPORT void operator delete[](void* p, std::align_val_t) EXCEPTSPEC
-template<typename New_Func, typename Del_Func>
-void test_new_delete_aligned(New_Func new_fun, Del_Func del_fun, size_t size)
+template<
+  void* (*new_fun)(size_t, std::align_val_t),
+  void (*del_fun)(void*, std::align_val_t) EXCEPTSPEC>
+void test_new_delete_aligned(size_t size)
 {
   for (auto& align_val_size : align_val_sizes)
   {
@@ -121,9 +119,7 @@ void test_new_delete_aligned(New_Func new_fun, Del_Func del_fun, size_t size)
       align_val_size);
     del_fun(aligned_mem, align_val);
 
-    test_zero_alloc(
-      [&align_val, &new_fun](size_t s) { return new_fun(s, align_val); },
-      [&align_val, &del_fun](void* p) { del_fun(p, align_val); });
+    test_zero_alloc([&align_val](size_t s) { return new_fun(s, align_val); });
   }
 }
 
@@ -131,27 +127,31 @@ void test_new_delete_aligned(New_Func new_fun, Del_Func del_fun, size_t size)
 // std::nothrow_t&) noexcept
 // SNMALLOC_EXPORT void* operator new[](size_t size,
 // std::align_val_t val, const std::nothrow_t&) noexcept
-template<typename New_Func, typename Del_Func>
-void test_new_delete_aligned_nothrow(New_Func new_fun, Del_Func del_fun)
+template<
+  void* (*new_fun)(size_t, std::align_val_t, const std::nothrow_t&),
+  void (*del_fun)(void*, std::align_val_t, const std::nothrow_t&) EXCEPTSPEC>
+void test_new_delete_aligned_nothrow()
 {
   std::align_val_t page_size{OS_PAGE_SIZE};
-  void* impossible_alloc_aligned = new_fun(static_cast<size_t>(-1), page_size);
+  void* impossible_alloc_aligned =
+    new_fun(static_cast<size_t>(-1), page_size, std::nothrow);
   EXPECT(
     impossible_alloc_aligned == nullptr,
     "Impossible allocation should not succeed");
-  del_fun(impossible_alloc_aligned, page_size);
+  del_fun(impossible_alloc_aligned, page_size, std::nothrow);
 
   test_zero_alloc(
-    [&page_size, &new_fun](size_t s) { return new_fun(s, page_size); },
-    [&page_size, &del_fun](void* p) { del_fun(p, page_size); });
+    [&page_size](size_t s) { return new_fun(s, page_size, std::nothrow); });
 }
 
 // SNMALLOC_EXPORT void operator delete(void* p, size_t size, std::align_val_t
 // val) EXCEPTSPEC
 // SNMALLOC_EXPORT void operator delete[](void* p, size_t size,
 // std::align_val_t val) EXCEPTSPEC
-template<typename New_Func, typename Del_Func>
-void test_delete_size_aligned(New_Func new_fun, Del_Func del_fun, size_t size)
+template<
+  void* (*new_fun)(size_t, std::align_val_t),
+  void (*del_fun)(void*, size_t, std::align_val_t) EXCEPTSPEC>
+void test_delete_size_aligned(size_t size)
 {
   for (auto& align_val_size : align_val_sizes)
   {
@@ -176,70 +176,34 @@ int main(int argc, char** argv)
   test_delete_null();
 
   START_TEST("Test new / delete simple");
-  test_new_delete_simple(
-    [](size_t s) { return operator new(s); },
-    [](void* p) { operator delete(p); },
-    42);
+  test_new_delete_simple < operator new, operator delete>(42);
   START_TEST("Test new[] / delete[] simple");
-  test_new_delete_simple(
-    [](size_t s) { return operator new[](s); },
-    [](void* p) { operator delete[](p); },
-    42);
+  test_new_delete_simple < operator new[], operator delete[]>(42);
 
   START_TEST("Test new / delete nothrow");
-  test_new_delete_nothrow(
-    [](size_t s) { return operator new(s, std::nothrow); },
-    [](void* p) { operator delete(p, std::nothrow); });
+  test_new_delete_nothrow < operator new, operator delete>();
   START_TEST("Test new[] / delete[] nothrow");
-  test_new_delete_nothrow(
-    [](size_t s) { return operator new[](s, std::nothrow); },
-    [](void* p) { operator delete[](p, std::nothrow); });
+  test_new_delete_nothrow < operator new[], operator delete[]>();
 
   START_TEST("Test delete with size parameter");
-  test_delete_size(
-    [](size_t s) { return operator new(s); },
-    [](void* p, size_t sz) { operator delete(p, sz); },
-    42);
+  test_delete_size < operator new, operator delete>(42);
   START_TEST("Test delete[] with size parameter");
-  test_delete_size(
-    [](size_t s) { return operator new[](s); },
-    [](void* p, size_t sz) { operator delete[](p, sz); },
-    42);
+  test_delete_size < operator new[], operator delete[]>(42);
 
   START_TEST("Test new / delete aligned");
-  test_new_delete_aligned(
-    [](size_t s, std::align_val_t a) { return operator new(s, a); },
-    [](void* p, std::align_val_t a) { operator delete(p, a); },
-    42);
+  test_new_delete_aligned < operator new, operator delete>(42);
   START_TEST("Test new[] / delete[] aligned");
-  test_new_delete_aligned(
-    [](size_t s, std::align_val_t a) { return operator new[](s, a); },
-    [](void* p, std::align_val_t a) { operator delete[](p, a); },
-    42);
+  test_new_delete_aligned < operator new[], operator delete[]>(42);
 
   START_TEST("Test non-throwing aligned new / delete");
-  test_new_delete_aligned_nothrow(
-    [](size_t s, std::align_val_t a) {
-      return operator new(s, a, std::nothrow);
-    },
-    [](void* p, std::align_val_t a) { operator delete(p, a, std::nothrow); });
+  test_new_delete_aligned_nothrow < operator new, operator delete>();
   START_TEST("Test non-throwing aligned new[] / delete[]");
-  test_new_delete_aligned_nothrow(
-    [](size_t s, std::align_val_t a) {
-      return operator new[](s, a, std::nothrow);
-    },
-    [](void* p, std::align_val_t a) { operator delete[](p, a, std::nothrow); });
+  test_new_delete_aligned_nothrow < operator new[], operator delete[]>();
 
   START_TEST("Test non-throwing aligned delete with explicit size");
-  test_delete_size_aligned(
-    [](size_t s, std::align_val_t a) { return operator new(s, a); },
-    [](void* p, size_t sz, std::align_val_t a) { operator delete(p, sz, a); },
-    42);
+  test_delete_size_aligned < operator new, operator delete>(42);
   START_TEST("Test non-throwing aligned delete[] with explicit size");
-  test_delete_size_aligned(
-    [](size_t s, std::align_val_t a) { return operator new[](s, a); },
-    [](void* p, size_t sz, std::align_val_t a) { operator delete[](p, sz, a); },
-    42);
+  test_delete_size_aligned < operator new[], operator delete[]>(42);
 
   snmalloc::debug_check_empty();
   return 0;
