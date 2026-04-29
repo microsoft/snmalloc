6d6c8f08dba34e798470d29a25a5c44f720e05bd
Paul Heidekrüger <paul.heidekrueger@tum.de>
2026-04-29 13:54:26 +0000

    Add tests for src/snmalloc/override/new.cc (#839)
    * Add intrastructure for new.cc tests
    
    * Test deletion of null pointers
    
    * Test simple allocation and deletion
    
    * Test simple allocation and deletion with `std::nothrow`
    
    * Test sized deletion
    
    * Test aligned allocation and deletion
    
    * Test aligned allocation with `std::nothrow`
    
    * Test aligned sized delete with `std::nothrow`
    
    Co-authored-by: Matthew Parkinson <mattpark@microsoft.com>

--- src/test/helpers.h
+++ src/test/helpers.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <cstddef>
+#include <cstdint>
 #ifdef _MSC_VER
 #  define __PRETTY_FUNCTION__ __FUNCSIG__
 #endif
@@ -36,4 +38,11 @@ namespace snmalloc
     DefaultPal::message(mb.get_message()); \
   } while (0)
 
+}
+
+// Based on:
+// https://en.cppreference.com/w/cpp/memory/is_sufficiently_aligned.html
+bool is_aligned(void* ptr, std::size_t align_val_size)
+{
+  return reinterpret_cast<std::uintptr_t>(ptr) % align_val_size == 0;
 }
